0316be151a7624dbcc4d840e40b65d33615a7780
tamsin <133944533+tamsin-milla@users.noreply.github.com>
2024-04-28 13:56:59 +1200

    Update 06 GAM.R

--- 06 GAM.R
+++ 06 GAM.R
@@ -20,7 +20,7 @@ gam_d_df <- manta_df %>% filter(period == "day")
 gam_d <- gam(g ~
               s(wind_direction, bs = 'cc', k = 6) +
               s(wind_speed, k = 6) +
-              s(altitude, k = 6) + 
+              s(sunaltitude, k = 6) + 
               s(kd490, k = 6) +
               s(bathy, k = 6) +
               s(sst, k = 6) +
@@ -38,12 +38,12 @@ plot(gam_d, all.terms = TRUE, pages = 1, scale = 0, shade = 1)
 gam_n_df <- manta_df %>% filter(period == "night")
 
 gam_n <- gam(g ~
-               s(wind_direction, bs = 'cc', k = 6) +
-               s(wind_speed, k = 6) +
+               s(wind_direction, bs = 'cc') +
+               s(wind_speed) +
                s(fraction, k = 6) + 
                s(kd490, k = 6) +
                s(bathy, k = 6) +
-               s(sst, k = 6) +
+               #s(sst, k = 6) +
                factor(tide_category) +
                factor(id),
              data = gam_n_df,
@@ -63,7 +63,6 @@ sm_d <- smooth_estimates(gam_d) %>%
 param_effects_d <- parametric_effects(gam_d)
 
 tide_category_effects_d <- subset(param_effects_d, term == "factor(tide_category)")
-sex_effects_d <- subset(param_effects_d, term == "factor(sex)")
 id_effects_d <- subset(param_effects_d, term == "factor(id)")
 
 ### BATHY ------------------------------------------------------------------------
@@ -73,7 +72,7 @@ a <- ggplot(sm_d, aes(y = est, x = bathy)) +
   geom_rug(data = gam_d_df, mapping = aes(x = bathy), sides = "b", inherit.aes = FALSE) +
   geom_ribbon(aes(ymin = lower_ci, ymax = upper_ci),
               alpha = 0.2, fill = "#a6cee3", colour = "#a6cee3", linetype = "dashed") +  # Add dashed border
-  geom_line(colour = "#a6cee3", size = 1.5) +
+  geom_line(colour = "#a6cee3", linewidth = 1.5) +
   labs(y = "Partial effect",
        x = "Bathymetry (m)") +
   scale_x_continuous(limits = c(-310, -40), labels = function(x) abs(x), breaks = seq(-300, -50, by = 100)) +  # Set x-axis limits, remove negative sign, and add x labels every 50m
@@ -136,8 +135,8 @@ c <- ggplot(sm_d, aes(y = est, x = sst)) +
 ### WindDir ----------------------------------------------------------------------
 # Plot smooth component estimates with confidence intervals using ggplot2
 # Your ggplot code
-d <- ggplot(sm_d, aes(y = est, x = wind_dir)) +
-  geom_rug(data = gam_d_df, mapping = aes(x = wind_dir), sides = "b", inherit.aes = FALSE) +
+d <- ggplot(sm_d, aes(y = est, x = wind_direction)) +
+  geom_rug(data = gam_d_df, mapping = aes(x = wind_direction), sides = "b", inherit.aes = FALSE) +
   geom_ribbon(aes(ymin = lower_ci, ymax = upper_ci),
               alpha = 0.2, fill = "#caa7cf", colour = "#caa7cf", linetype = "dashed") +  # Add dashed border
   geom_line(colour = "#caa7cf", size = 1.5) +
@@ -166,7 +165,7 @@ e <- ggplot(sm_d, aes(y = est, x = wind_speed)) +
   geom_line(colour = "#a6cee3", size = 1.5) +
   labs(y = "Partial effect",
        x = expression("Wind speed (ms"^{-1} * ")")) +  # Include (ms^-1) in brackets
-  scale_x_continuous(limits = c(2, 16.9)) +
+  scale_x_continuous(limits = c(2, 14)) +
   theme_minimal() +
   theme(panel.background = element_blank(),  # Remove panel background
         panel.grid.major = element_blank(),  # Remove major grid lines
@@ -183,14 +182,14 @@ e <- ggplot(sm_d, aes(y = est, x = wind_speed)) +
 ### ALTITUDE -------------------------------------------------------------------
 # Plot smooth component estimates with confidence intervals using ggplot2
 # Your ggplot code
-f <- ggplot(sm_d, aes(y = est, x = altitude)) +
-  geom_rug(data = gam_d_df, mapping = aes(x = altitude), sides = "b", inherit.aes = FALSE) +
+f <- ggplot(sm_d, aes(y = est, x = sunaltitude)) +
+  geom_rug(data = gam_d_df, mapping = aes(x = sunaltitude), sides = "b", inherit.aes = FALSE) +
   geom_ribbon(aes(ymin = lower_ci, ymax = upper_ci),
               alpha = 0.2, fill = "#a6cee3", colour = "#a6cee3", linetype = "dashed") +  # Add dashed border
   geom_line(colour = "#a6cee3", size = 1.5) +
   labs(y = NULL,
        x = "Altitude (°)") +
-  coord_cartesian(ylim = c(-0.5, 0.5)) +  # Adjust y-axis limits
+  coord_cartesian(ylim = c(-2, 2)) +  # Adjust y-axis limits
   theme_minimal() +
   theme(panel.background = element_blank(),  # Remove panel background
         panel.grid.major = element_blank(),  # Remove major grid lines
@@ -221,26 +220,9 @@ g <- ggplot(tide_category_effects_d, aes(x = level, y = partial)) +
         axis.title = element_text(size = 10, color = "black", family = "Arial")) +  # Adjust axis title properties
   theme(axis.ticks.length = unit(-0.2, "cm"))  # Move ticks outside the plot
 
-
-### SEX -----------------------------------------------------------------------
-# Plot the parametric effects with circles and error bars
-h <- ggplot(sex_effects_d, aes(x = level, y = partial)) +
-  geom_errorbar(aes(ymin = partial - se, ymax = partial + se), width = 0.2, size = 1) +  # Error bars
-  geom_point(shape = 21, size = 5, fill = "black") +  # Circle for the partial effect
-  labs(x = "Sex", y = NULL) +
-  coord_cartesian(ylim = c(-1, 1)) +  # Adjust y-axis limits
-  theme_minimal() +
-  theme(panel.grid.major = element_blank(),  # Remove major grid lines
-        panel.grid.minor = element_blank(),  # Remove minor grid lines
-        panel.background = element_blank(),  # Remove panel background
-        axis.line = element_line(color = "black"),  # Add axis lines
-        axis.text = element_text(size = 10, color = "black", family = "Arial"),  # Adjust axis text properties
-        axis.title = element_text(size = 10, color = "black", family = "Arial")) +  # Adjust axis title properties
-  theme(axis.ticks.length = unit(-0.2, "cm"))  # Move ticks outside the plot
-
 ### ID -------------------------------------------------------------------------
 # Plot the parametric effects with circles and error bars
-i <- ggplot(id_effects_d, aes(x = level, y = partial)) +
+h <- ggplot(id_effects_d, aes(x = level, y = partial)) +
   geom_errorbar(aes(ymin = partial - se, ymax = partial + se), width = 0.2, size = 1) +  # Error bars
   geom_point(fill = "black", shape = 21, size = 5) +  # Circle for the partial effect
   labs(x = "PTT #", y = "Partial effect") +
@@ -256,11 +238,9 @@ i <- ggplot(id_effects_d, aes(x = level, y = partial)) +
   guides(fill = FALSE)  # Remove legend
 
 
-
-
 ### PLOTTING FIGURES -----------------------------------------------------------
 # Combine all plots into one figure
-combined_plot_d <- (a | b | c | d | e | f | g | h | i ) +
+combined_plot_d <- (a | b | c | d | e | f | g | h ) +
   plot_layout(ncol = 2, nrow = 5)  # Arrange plots in two columns
 
 # Display the combined plot
@@ -275,7 +255,6 @@ sm_n <- smooth_estimates(gam_n) %>%
 param_effects_n<- parametric_effects(gam_n)
 
 tide_category_effects_n <- subset(param_effects_n, term == "factor(tide_category)")
-sex_effects_n <- subset(param_effects_n, term == "factor(sex)")
 id_effects_n <- subset(param_effects_n, term == "factor(id)")
 
 ### BATHY ----------------------------------------------------------------------
@@ -323,7 +302,7 @@ b <- ggplot(sm_n, aes(y = est, x = kd490)) +
 ### SST ------------------------------------------------------------------------
 # Plot smooth component estimates with confidence intervals using ggplot2
 # Your ggplot code
-c <- ggplot(sm_n, aes(y = est, x = sst)) +
+#c <- ggplot(sm_n, aes(y = est, x = sst)) +
   geom_rug(data = gam_n_df, mapping = aes(x = sst), sides = "b", inherit.aes = FALSE) +
   geom_ribbon(aes(ymin = lower_ci, ymax = upper_ci),
               alpha = 0.2, fill = "#a6cee3", colour = "#a6cee3", linetype = "dashed") +  # Add dashed border
@@ -349,8 +328,8 @@ c <- ggplot(sm_n, aes(y = est, x = sst)) +
 ### WindDir ----------------------------------------------------------------------
 # Plot smooth component estimates with confidence intervals using ggplot2
 # Your ggplot code
-d <- ggplot(sm_n, aes(y = est, x = wind_dir)) +
-  geom_rug(data = gam_n_df, mapping = aes(x = wind_dir), sides = "b", inherit.aes = FALSE) +
+d <- ggplot(sm_n, aes(y = est, x = wind_direction)) +
+  geom_rug(data = gam_n_df, mapping = aes(x = wind_direction), sides = "b", inherit.aes = FALSE) +
   geom_ribbon(aes(ymin = lower_ci, ymax = upper_ci),
               alpha = 0.2, fill = "#a6cee3", colour = "#a6cee3", linetype = "dashed") +  # Add dashed border
   geom_line(colour = "#a6cee3", size = 1.5) +
@@ -366,7 +345,7 @@ d <- ggplot(sm_n, aes(y = est, x = wind_dir)) +
         axis.ticks.margin = unit(0.5, "cm"),  # Set margin between tick marks and plot area
         axis.title = element_text(family = "Arial", size = 10, color = "black"),  # Set axis title properties
         axis.text = element_text(family = "Arial", size = 10, color = "black")) +  # Set axis text properties
-  coord_cartesian(ylim = c(-0.3, 0.3))  # Set y-axis limits
+  coord_cartesian(ylim = c(-1.6, 1.6))  # Set y-axis limits
 
 
 ### WindSpeed --------------------------------------------------------------------
@@ -392,14 +371,11 @@ e <- ggplot(sm_n, aes(y = est, x = wind_speed)) +
         axis.text = element_text(family = "Arial", size = 10, color = "black"))  +  # Set axis text properties
   coord_cartesian(ylim = c(-1, 1))  # Set y-axis limits
 
-
-
-
 ### MOON FRACTION --------------------------------------------------------------
 # Plot smooth component estimates with confidence intervals using ggplot2
 # Your ggplot code
-f <- ggplot(sm_n, aes(y = est, x = moonfraction.fraction)) +
-  geom_rug(data = gam_n_df, mapping = aes(x = moonfraction.fraction), sides = "b", inherit.aes = FALSE) +
+f <- ggplot(sm_n, aes(y = est, x = fraction)) +
+  geom_rug(data = gam_n_df, mapping = aes(x = fraction), sides = "b", inherit.aes = FALSE) +
   geom_ribbon(aes(ymin = lower_ci, ymax = upper_ci),
               alpha = 0.2, fill = "#a6cee3", colour = "#a6cee3", linetype = "dashed") +  # Add dashed border
   geom_line(colour = "#a6cee3", size = 1.5) +
@@ -416,7 +392,6 @@ f <- ggplot(sm_n, aes(y = est, x = moonfraction.fraction)) +
         axis.ticks.margin = unit(0.5, "cm"),  # Set margin between tick marks and plot area
         axis.title = element_text(family = "Arial", size = 10, color = "black"),  # Set axis title properties
         axis.text = element_text(family = "Arial", size = 10, color = "black"))  # Set axis text properties
-
 
 
 ### TIDE -----------------------------------------------------------------------
@@ -439,26 +414,9 @@ g <- ggplot(tide_category_effects_n, aes(x = level, y = partial)) +
   theme(axis.ticks.length = unit(-0.2, "cm"))  # Move ticks outside the plot
 
 
-### SEX -----------------------------------------------------------------------
-# Plot the parametric effects with circles and error bars
-h <- ggplot(sex_effects_n, aes(x = level, y = partial)) +
-  geom_errorbar(aes(ymin = partial - se, ymax = partial + se), width = 0.2, size = 1) +  # Error bars
-  geom_point(shape = 21, size = 5, fill = "black") +  # Circle for the partial effect
-  labs(x = "Sex", y = NULL) +
-  coord_cartesian(ylim = c(-1, 1)) +  # Adjust y-axis limits
-  theme_minimal() +
-  theme(panel.grid.major = element_blank(),  # Remove major grid lines
-        panel.grid.minor = element_blank(),  # Remove minor grid lines
-        panel.background = element_blank(),  # Remove panel background
-        axis.line = element_line(color = "black"),  # Add axis lines
-        axis.text = element_text(size = 10, color = "black", family = "Arial"),  # Adjust axis text properties
-        axis.title = element_text(size = 10, color = "black", family = "Arial")) +  # Adjust axis title properties
-  theme(axis.ticks.length = unit(-0.2, "cm"))  # Move ticks outside the plot
-
-
 ### ID -------------------------------------------------------------------------
 # Plot the parametric effects with circles and error bars
-i <- ggplot(id_effects_n, aes(x = level, y = partial)) +
+h <- ggplot(id_effects_n, aes(x = level, y = partial)) +
   geom_errorbar(aes(ymin = partial - se, ymax = partial + se), width = 0.2, size = 1) +  # Error bars
   geom_point(fill = "black", shape = 21, size = 5) +  # Circle for the partial effect
   labs(x = "PTT #", y = "Partial effect") +
@@ -474,10 +432,9 @@ i <- ggplot(id_effects_n, aes(x = level, y = partial)) +
   guides(fill = FALSE)  # Remove legend
 
 
-
 ### PLOTTING FIGURES -------------------------------------------------------------------------
 # Combine all plots into one figure
-combined_plot_n <- (a | b | c | d | e | f | g | h | i ) +
+combined_plot_n <- (a | b  | d | e | f | g | h ) +
   plot_layout(ncol = 2, nrow = 5)  # Arrange plots in two columns
 
 # Display the combined plot
