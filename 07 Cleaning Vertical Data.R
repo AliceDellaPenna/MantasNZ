39b4be67802f06acc16b6295ea52a98cfd6650fa
AliceDellaPenna <alice.dellapenna@gmail.com>
2025-04-11 09:53:50 +1200

    Update 07 Cleaning Vertical Data.R
    Update with new tracking data from Tamsin

--- 07 Cleaning Vertical Data.R
+++ 07 Cleaning Vertical Data.R
@@ -6,7 +6,7 @@ library(purrr); library(tidyr)
 ## CLEAN DATA (UNRECOVERED TAGS) ===============================================
 
 # List all CSV files in the directory
-file_list <- list.files(path = "Vertical_Unrecovered_Raw/", pattern = "\\.csv$", full.names = TRUE)
+file_list <- list.files(path = "/Users/tamsin/Files/Manuscript/Data/Vertical_Unrecovered_Raw/", pattern = "\\.csv$", full.names = TRUE)
 
 # Loop through each file
 for (file in file_list) {
@@ -30,7 +30,7 @@ for (file in file_list) {
   
   # Keep only the desired columns
   data <- data %>%
-    select(Ptt, datetime, local.time, Depth, Temperature)
+    dplyr::select(Ptt, datetime, local.time, Depth, Temperature)
   
   # Convert Ptt column to character type
   data$Ptt <- as.character(data$Ptt)
@@ -45,15 +45,15 @@ for (file in file_list) {
 ## CLEAN DATA (RECOVERED TAGS) =================================================
 
 # List all CSV files in the directory
-file_list <- list.files(path = "Vertical_Recovered_Raw", pattern = "\\.csv$", full.names = TRUE)
+file_list <- list.files(path = "/Users/tamsin/Files/Manuscript/Data/Vertical_Recovered_Raw", pattern = "\\.csv$", full.names = TRUE)
 
 # Create an empty list to store data frames
 recovered_dfs <- list()
+
 # Loop through each file and read it into a data frame
 for (file in file_list) {
   recovered_dfs[[file]] <- read.csv(file)
 }
-
 
 # Loop through each file in the recovered_dfs list
 for (file_name in names(recovered_dfs)) {
@@ -82,7 +82,7 @@ for (file_name in names(recovered_dfs)) {
   
   # Keep only the desired columns
   recovered_dfs[[file_name]] <- recovered_dfs[[file_name]] %>%
-    select(Ptt, datetime, local.time, Corrected.Depth, External.Temperature)
+    dplyr::select(Ptt, datetime, local.time, Corrected.Depth, External.Temperature)
   
   # Rename columns
   recovered_dfs[[file_name]] <- 
@@ -96,32 +96,36 @@ for (file_name in names(recovered_dfs)) {
   assign(df_name, recovered_dfs[[file_name]])
 }
 
-
 ## SUBSET DATA =================================================================
-
 # Cutoff data following final confirmed GPS position
-df_197235 <- df_197235 %>% filter(datetime <= as.POSIXct("2021-03-09 06:29:19", tz = "UTC"))
+df_197235 <- df_197235 %>% filter(datetime <= as.POSIXct("2021-03-06 09:48:34", tz = "UTC"))
 df_204511 <- df_204511 %>% filter(datetime <= as.POSIXct("2022-02-07 08:06:24", tz = "UTC"))
-df_238014 <- df_238014 %>% filter(datetime <= as.POSIXct("2023-02-13 06:37:28", tz = "UTC"))
+df_215016 <- df_215016 %>% filter(datetime <= as.POSIXct("2022-03-01 13:10:19", tz = "UTC"))
+df_238014 <- df_238014 %>% filter(datetime <= as.POSIXct("2023-02-13 05:40:41", tz = "UTC"))
 df_238015 <- df_238015 %>% filter(datetime <= as.POSIXct("2023-02-02 06:47:44", tz = "UTC"))
 df_238016 <- df_238016 %>% filter(datetime <= as.POSIXct("2023-02-11 23:10:00", tz = "UTC"))
 df_238018 <- df_238018 %>% filter(datetime <= as.POSIXct("2023-01-31 20:41:53", tz = "UTC"))
-df_238019 <- df_238019 %>% filter(datetime <= as.POSIXct("2023-04-04 19:27:06", tz = "UTC"))
+df_238019 <- df_238019 %>% filter(datetime <= as.POSIXct("2023-03-28 19:14:46", tz = "UTC"))
 df_252520 <- df_252520 %>% filter(datetime <= as.POSIXct("2024-02-02 07:12:05", tz = "UTC"))
-df_252522 <- df_252522 %>% filter(datetime <= as.POSIXct("2024-02-02 06:58:48", tz = "UTC"))
+df_252522 <- df_252522 %>% filter(datetime <= as.POSIXct("2024-03-15 19:52:18", tz = "UTC"))
+df_252524 <- df_252524 %>% filter(datetime <= as.POSIXct("2024-03-14 04:04:34", tz = "UTC"))
 df_252525 <- df_252525 %>% filter(datetime <= as.POSIXct("2024-02-29 03:07:28", tz = "UTC"))
-df_252528 <- df_252528 %>% filter(datetime <= as.POSIXct("2024-02-22 03:43:16", tz = "UTC"))
-df_252778 <- df_252778 %>% filter(datetime <= as.POSIXct("2024-02-18 07:43:13", tz = "UTC"))
+df_252526 <- df_252526 %>% filter(datetime <= as.POSIXct("2024-03-12 08:01:10", tz = "UTC"))
+df_252528 <- df_252528 %>% filter(datetime <= as.POSIXct("2024-03-12 04:34:05", tz = "UTC"))
+df_252778 <- df_252778 %>% filter(datetime <= as.POSIXct("2024-02-18 07:45:16", tz = "UTC"))
 df_252779 <- df_252779 %>% filter(datetime <= as.POSIXct("2024-02-13 07:41:27", tz = "UTC"))
 
 # Cutoff data prior to deployment on animal
 df_197235 <- df_197235 %>% filter(datetime >= as.POSIXct("2021-02-03 00:58:00", tz = "UTC"))
 df_204511 <- df_204511 %>% filter(datetime >= as.POSIXct("2022-01-10 04:15:00", tz = "UTC"))
+df_215016 <- df_215016 %>% filter(datetime >= as.POSIXct("2022-02-07 03:27:00", tz = "UTC"))
 df_238014 <- df_238014 %>% filter(datetime >= as.POSIXct("2023-01-22 01:30:00", tz = "UTC"))
 df_238015 <- df_238015 %>% filter(datetime >= as.POSIXct("2023-01-22 06:40:00", tz = "UTC"))
 df_238016 <- df_238016 %>% filter(datetime >= as.POSIXct("2023-01-22 04:05:00", tz = "UTC"))
 df_238018 <- df_238018 %>% filter(datetime >= as.POSIXct("2023-01-22 05:40:00", tz = "UTC"))
 df_238019 <- df_238019 %>% filter(datetime >= as.POSIXct("2023-01-22 06:00:00", tz = "UTC"))
+df_252524 <- df_252524 %>% filter(datetime >= as.POSIXct("2023-12-28 03:34:16", tz = "UTC"))
+df_252528 <- df_252528 %>% filter(datetime >= as.POSIXct("2023-12-27 03:51:19", tz = "UTC"))
 
 df_252520 <- df_252520 %>% filter(local.time >= as.POSIXct("2023-12-27 14:43:00", tz = "Pacific/Auckland"))
 df_252522 <- df_252522 %>% filter(local.time >= as.POSIXct("2023-12-27 15:27:00", tz = "Pacific/Auckland"))
@@ -132,10 +136,33 @@ df_252779 <- df_252779 %>% filter(local.time >= as.POSIXct("2024-01-04 18:04:00"
 
 
 ## SAVE FILES ==================================================================
-setwd("RDA_files") #Set working directory to save RDA file
+
+# Define the folder path
+folder_path <- "/Users/tamsin/Files/Manuscript/Data/Vertical_Cleaned"
+
+# Save each filtered data frame as a CSV file in the folder
+write.csv(df_197235, file.path(folder_path, "df_197235_filtered.csv"), row.names = FALSE)
+write.csv(df_204511, file.path(folder_path, "df_204511_filtered.csv"), row.names = FALSE)
+write.csv(df_215016, file.path(folder_path, "df_215016_filtered.csv"), row.names = FALSE)
+write.csv(df_238014, file.path(folder_path, "df_238014_filtered.csv"), row.names = FALSE)
+write.csv(df_238015, file.path(folder_path, "df_238015_filtered.csv"), row.names = FALSE)
+write.csv(df_238016, file.path(folder_path, "df_238016_filtered.csv"), row.names = FALSE)
+write.csv(df_238018, file.path(folder_path, "df_238018_filtered.csv"), row.names = FALSE)
+write.csv(df_238019, file.path(folder_path, "df_238019_filtered.csv"), row.names = FALSE)
+write.csv(df_252520, file.path(folder_path, "df_252520_filtered.csv"), row.names = FALSE)
+write.csv(df_252522, file.path(folder_path, "df_252522_filtered.csv"), row.names = FALSE)
+write.csv(df_252524, file.path(folder_path, "df_252524_filtered.csv"), row.names = FALSE)
+write.csv(df_252525, file.path(folder_path, "df_252525_filtered.csv"), row.names = FALSE)
+write.csv(df_252526, file.path(folder_path, "df_252526_filtered.csv"), row.names = FALSE)
+write.csv(df_252528, file.path(folder_path, "df_252528_filtered.csv"), row.names = FALSE)
+write.csv(df_252778, file.path(folder_path, "df_252778_filtered.csv"), row.names = FALSE)
+write.csv(df_252779, file.path(folder_path, "df_252779_filtered.csv"), row.names = FALSE)
+
+setwd("/Users/tamsin/Files/Manuscript/RDA_files") #Set working directory to save RDA file
 
 save(df_197235, 
      df_204511,
+     df_215016,
      df_238014,
      df_238015,
      df_238016,
@@ -143,7 +170,9 @@ save(df_197235,
      df_238019,
      df_252520,
      df_252522,
+     df_252524,
      df_252525,
+     df_252526,
      df_252528,
      df_252778,
      df_252779, 
